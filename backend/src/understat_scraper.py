e32f5072fb7343ec5b492d5f1d31fc41b1293b51
Jamie Valentine <jamievalentine191919@gmail.com>
2026-02-10 13:12:36 +0000

    Begun testing how to scrape to fine teams in a table

---
+++ backend/src/understat_scraper.py
@@ -0,0 +1,93 @@
+import json
+import os
+import requests
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import pandas as pd
+
+# Setup for Selenium
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+# TODO
+service = ChromeService(executable_path=r"C:\Users\jamie\Programming_Projects\_dependencies\chromedriver-win64\chromedriver.exe")
+
+# region Constants
+
+DATA_DIR = "data/"
+
+LEAGUE_URL = "https://understat.com/league/EPL"
+TEAM_URL = "https://understat.com/team/"
+PLAYER_URL = "https://understat.com/player/"
+
+
+
+# endregion Constants
+
+# region Entry Point
+
+def main():
+    current_dir = os.getcwd()
+    data_dir = current_dir.rsplit('\\', 1)[0] + "data/"
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    selenium_scraping()
+    #html_scraping()
+
+# region Entry Point
+
+# region Functions
+
+def selenium_scraping():
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.get(LEAGUE_URL)
+    try:
+        wait = WebDriverWait(driver, 10)
+        #table = wait.until(EC.presence_of_element_located((By.ID, "league-chemp")))
+        table = wait.until(EC.presence_of_element_located((By.TAG_NAME, "table")))
+
+        html_table = table.get_attribute("outerHTML")
+        #html_table_block = table.get_attribute("innerHTML")
+        #table_start_index = html_table_block.find("<table")
+        #table_end_index = html_table_block.find("</table>")
+        #html_table = html_table_block[table_start_index:table_end_index + len("</table>")]
+
+        teams_list = []
+        name_index = html_table.find("<a href=")
+
+        while name_index != -1:
+            html_table = html_table[name_index + len("<a href="):]
+
+        print(html_table.find("<a href="))
+        print(html_table[1019:1119])
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
+
+def html_scraping():
+    page = urlopen(LEAGUE_URL)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    league_index = html.find("<div id=\"league-chemp\"")
+    print(league_index)
+    print(html[league_index:])
+
+def get_teams():
+
+    pass
+
+# region Functions
+
+if __name__ == "__main__":
+    main()
